a5fa9112d90d2ba0b3575602a37d2716fa913e77
Tito Ayala <titoayala@gmail.com>
2022-09-13 14:24:32 -0400

    Combinando view y component CONTACTO.JSX

--- src/views/Contacto.jsx
+++ src/views/Contacto.jsx
@@ -1,11 +1,25 @@
 import { Container } from "react-bootstrap";
-import Contacto from "../components/Contacto";
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 
 export default () => {
     return (
-        <Container className="pt-5 contacto">
+        <Container className="pt-5">
             <h1 className="mb-4">Cuéntanos, ¿en qué te podemos ayudar?</h1>
-            <Contacto />
+            <Form>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>Correo:</Form.Label>
+                    <Form.Control type="email" placeholder="Ingrese Correo electrónico" />
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+                    <Form.Label>Descripción</Form.Label>
+                    <Form.Control as="textarea" rows={3} />
+                </Form.Group>
+
+                <Button variant="danger" type="submit">Enviar</Button>
+
+            </Form>
         </Container>
     );
 };
